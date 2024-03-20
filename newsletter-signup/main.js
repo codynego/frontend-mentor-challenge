e19191f77fe3bd2e0f9aafc90a96afb47b45ce3a
abednego emonena <emonnenaabednego@gmail.com>
2024-03-20 23:02:09 +0100

    finally fix success page not responsive and getting data

--- newsletter-signup/main.js
+++ newsletter-signup/main.js
@@ -1,14 +1,24 @@
+const successPage = document.getElementById("success")
+
+console.log(successPage)
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const nname = urlParams.get('email');
+console.log("hey there")
+console.log(nname)
+successPage.innerText = nname
+
+
 const butt= document.getElementById('btn');
 const emailInput = document.getElementById("emailpl")
 
 
 const handleSubmit = (emailInput) => {
   const value = emailInput.value;
-  console.log(value);
 }
 
 butt.addEventListener('click', (e) => {
-  console.log("hello")
   handleSubmit(emailInput)
 })
 
